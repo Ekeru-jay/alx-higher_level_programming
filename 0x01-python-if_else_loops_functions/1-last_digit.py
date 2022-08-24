01fb3cd7826c0407e707400afa550e82587478ef
Ekeru Onyeka James <safexexport01@gmail.com>
2022-08-24 15:35:51 -0700

    my first else_loop

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,9 @@ if number > 0:
     id = number % 10
 else:
     id = number % -10
-
-    if id > 5:
-        print("Last digit of {:d is {:d and is greater \ than 5" .format(number, id))
-    elseif id < 6 and id != 0:
-        print("Last digit of {:d} is {:d} and is less than 6 \ and not 0" .format(number, id))
-    else:
-        print("Last digit of {:d} is {:d} and is 0".format(number, ld))
+if id > 5:
+    print("Last digit of {:d is {:d and is greater \ than 5" .format(number, id))
+else if id < 6 and id != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 \ and not 0" .format(number, id))
+else:
+    print("Last digit of {:d} is {:d} and is 0".format(number, ld))
